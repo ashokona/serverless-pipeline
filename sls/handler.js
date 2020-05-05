a61015b7539d6107e2cbb1d2e8df503a4b9c920d
ashok k <ashokona@gmail.com>
2020-05-06 00:17:43 +0530

    Update handler.js

--- sls/handler.js
+++ sls/handler.js
@@ -3,6 +3,10 @@
 module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
       message: `Hello, the current time is ${new Date().toTimeString()}.`,
       event: event.headers
